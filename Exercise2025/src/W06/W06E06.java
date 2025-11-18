1bd2a447b574cfb7570072670a4e74fe35b892c9
zoeywongzixin <23118969@siswa.um.edu.my>
2025-11-18 11:47:49 +0800

    Add W06E06.java exercise

---
+++ Exercise2025/src/W06/W06E06.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package W06;
+
+/**
+ *
+ * @author User
+ */
+public class W06E06 {
+    public static void main(String[] args) {
+        String [] str= {"Ang","Tan","Fong","Ahmad","Ali"};
+
+
+        for(int i=0;i<str.length;i++){
+            System.out.print(str[i]+ " \n");
+        }
+    }
+    
+}
